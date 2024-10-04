dddfeca7e3ee71dc2b5b332c7bd2d53e7e970fcd
[Cristian Godoy] <[cgodoy@gmail.com]>
2024-10-04 11:23:16 +0200

    config as dict not yml

--- core_components/config/__init__.py
+++ core_components/config/__init__.py
@@ -1,11 +1,19 @@
-import yaml
-import os
-
+config = {
+    'global': {
+        'crs_map': 4326,
+        'crs_default': 25833
+    },
+    'hoydedata': {
+        'hoydedata_layer': "NHM_DTM_25833",
+        'hoydedata_url': "https://hoydedata.no/arcgis/rest/services/{}/ImageServer/exportImage?bbox={},{},{},{}&size={},{}&bboxSR=&size=&imageSR=&time=&format=tiff&pixelType=F32&noData={}&noDataInterpretation=esriNoDataMatchAny&interpolation=+RSP_BilinearInterpolation&compression=&compressionQuality=&bandIds=&mosaicRule=&renderingRule=&f=image"
+    },
+    'buildings': {
+        'url': "https://wfs.geonorge.no/skwms1/wfs.matrikkelen-bygningspunkt",
+        'columns': ['bygningsnummer', 'bygningsstatus', 'kommunenavn', 'bygningstype', 'bygningId', 'geometry'],
+        'query_filter': "bygningsstatus not in ('GR', 'IP', 'BR', 'BF', 'IG')"
+    }
+}
 
 def get_config():
-    basedir = os.path.dirname(os.path.realpath(__file__))
-    config_path = os.path.join(basedir, 'config.yml')
-    with open(config_path, 'r') as f:
-        config = yaml.safe_load(f)
     return config
     
