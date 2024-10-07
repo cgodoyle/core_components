7e30612aa9d48e78c9d5cb6dcfd50e0e7feaf9af
[Cristian Godoy] <[cgodoy@gmail.com]>
2024-10-07 10:45:36 +0200

    updated logger for .notebooks

--- core_components/logger/__init__.py
+++ core_components/logger/__init__.py
@@ -4,13 +4,12 @@ from pathlib import Path
 
 def setup_logger(name='root', log_file=None):
     current_dir = Path.cwd()
-    log_dir = Path('../.logs') if current_dir.name == "notebooks" else Path('.logs')
+    log_dir = Path('../.logs') if current_dir.name in ("notebooks", ".notebooks") else Path('.logs')
     
     if not log_dir.exists():
         log_dir.mkdir(parents=True)
 
     log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    date_format = '%Y-%m-%d %H:%M:%S'
 
     if log_file is None:
         log_file = log_dir / f'log_{datetime.now().strftime("%Y%m%d")}.log'
