b6a2857ad98ab24140869d9d7ea897351f6ac69b
[Cristian Godoy] <[cgodoy@gmail.com]>
2024-10-07 10:45:25 +0200

    updated config related to nadag

--- core_components/config/__init__.py
+++ core_components/config/__init__.py
@@ -15,7 +15,64 @@ config = {
     'consequence': {
         'url': "https://gis3.nve.no/arcgis/rest/services/geoprocessing/Konsekevens1/GPServer/KonskvensParametere1",
         'items': ["Beboere","Barn","Ansatte","Bygninger","Kraftlinjer","Toglinjer"]
-    }
+    },
+    'nadag': {
+        'url': "https://ogcapitest.ngu.no/rest/services/grunnundersokelser_utvidet/collections",
+        'column_mapper_borehole': {
+            'anvendtlast': 'penetration_force', 
+            'boretlengde': 'depth', 
+            'dreiemoment': 'rotation_moment', # ????
+            'kombinasjonsondering_fk': 'method_id',
+            'trykksondering_fk': 'method_id',
+            'statisksondering_fk': 'method_id',
+            'nedpressinghastighet': 'penetration_rate', 
+            'nedpressingtrykk': 'qc', # ???? <--- cpt
+            'friksjon': 'fs', # ???? <--- cpt
+            'poretrykk': 'u2', # ???? <--- cpt
+            'nedpressingtid': 'penetration_time', # ????
+            'observasjonkode': 'comment_code', 
+            'observasjonmerknad': 'comment', 
+            'rotasjonhastighet': 'rotation_rate',
+            'slagfrekvens': 'hammering_rate', # ????
+            'spylemengde': 'flushing_flow', 
+            'spyletrykk': 'flushing_pressure'},
+        'columns_samples': [
+            'lagPosisjon', 'prøveMetode_x', 'labAnalyse', 'boretLengde_x', 'geotekniskproveseriedel', 
+            'observasjonKode', 'skjærfasthetOmrørt', 'skjærfasthetUforstyrret', 'vanninnhold', 
+            'aksielDeformasjon', 'skjærfasthetUdrenert', 'detaljertLagSammensetning', 'densitetPrøvetaking', 
+            'flyteGrense', 'plastitetsGrense', 'prøveseriedelNavn', 'fraLengde', 'tilLengde', 'prøveseriedelId', 
+            'prøveserieId', 'geotekniskborehullunders', 'geometry', 'opphav', 'høydeReferanse', 'opprettetDato', 
+            'geotekniskMetode', 'boretAzimuth', 'boretHelningsgrad', 'boretLengdeTilBerg', 'undersøkelseStart', 
+            'høyde', 'forboretLengde', 'stoppKode', 'forboretStartLengde', 'borenr'
+        ],
+        'column_mapper_samples': {
+            'prøvemetode_x': 'prøvemetode',
+            'boretlengde_x': 'boretlengde',
+            'prøveseriedelnavn': 'name',
+            'geotekniskborehullunders':'location_id',
+            'fralengde': 'depth_top',
+            'tillengde': 'depth_base',
+            'skjærfasthetuforstyrret': 'strength_undisturbed',
+            'skjærfasthetudrenert': 'strength_undrained',
+            'skjærfasthetomrørt': 'strength_remoulded',
+            'flytegrense': 'liquid_limit',
+            'plastitetsgrense': 'plastic_limit',
+            'vanninnhold': 'water_content',
+            'høyde': 'location_elevation',
+            'detaljertlagsammensetning': 'layer_composition',
+            'borenr': 'location_name',},
+        'flag_codes': {
+                      'hammering_starts': ["11", "15", "63"],
+                      'hammering_ends': ["16", "64"],
+                      'increased_rotation_rate_starts': ["51"],
+                      'increased_rotation_rate_ends': ["52"],
+                      'flushing_starts': ["14", "63"],
+                      'flushing_ends': ["62", "64"],
+
+
+        }
+        
+    },
 }
 
 def get_config():
